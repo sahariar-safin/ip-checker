c6698a6c7a70ed3dca56104f7a063e5f565f74fc
Shafin The Developer <sahariar350@gmail.com>
2021-02-20 00:12:22 +0600

    Done man done!

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ fetch('https://api.ipify.org?format=json')
 
 const ipdetail = (getIp) => {
     const ip = getIp + "";
-    console.log(ip);
     fetch(`https://ipapi.co/${ ip }/json/`)
         .then(res => res.json())
         .then(data => handleDetails(data));
 }
 
 const handleDetails = (data) => {
-    console.log(data);
     const detailsContainer = document.getElementById('details');
     const details = document.createElement('div');
     details.className = 'details-container'
@@ -20,7 +18,7 @@ const handleDetails = (data) => {
                 <h3 class="m-1">Your Ip version: ${data.version}</h3>
                 <h3 class="m-1">City: ${data.city}</h3>
                 <h3 class="m-1">Region: ${data.region}</h3>
-                <h3 class="m-1">Region COde: ${data.region_code}</h3>
+                <h3 class="m-1">Region Code: ${data.region_code}</h3>
                 <h3 class="m-1">Country: ${data.country_name}</h3>
                 <h3 class="m-1">Country Code: ${data.country_code}</h3>
                 <h3 class="m-1">Postal Code: ${data.postal}</h3>
